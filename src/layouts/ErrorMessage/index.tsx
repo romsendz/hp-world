9fc8c7521f6b1bf746bb49e4aea98ee405eda03a
rsendziuk <sendzyuk.r@gmail.com>
2025-01-11 10:45:18 +0100

    render fallbacks based on fetch status

---
+++ src/layouts/ErrorMessage/index.tsx
@@ -0,0 +1,18 @@
+import styles from "./index.module.css";
+
+interface ErrorMessageProps {
+  error: Error | null;
+}
+
+const ErrorMessage = ({ error }: ErrorMessageProps) => {
+  return (
+    <div className={styles.error}>
+      <p>Something went wrong while fetching data...</p>
+      <p>Please try again</p>
+      <hr />
+      <p>Error: {error?.message}</p>
+    </div>
+  );
+};
+
+export default ErrorMessage;
---
+++ src/layouts/Loading/index.tsx
@@ -0,0 +1,5 @@
+const Loading = () => {
+  return <h3 style={{ textAlign: "center" }}>Loading...</h3>;
+};
+
+export default Loading;
