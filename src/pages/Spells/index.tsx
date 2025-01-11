5db2bc0ed383b6518ace7e8837caf78104701de3
rsendziuk <sendzyuk.r@gmail.com>
2025-01-11 10:44:28 +0100

    rendered spells

--- src/pages/Spells/index.tsx
+++ src/pages/Spells/index.tsx
@@ -1,9 +1,11 @@
+import SpellsList from "../../components/SpellsList";
 import Title from "../../components/Title";
 
 const Spells = () => {
   return (
     <>
       <Title>Spells</Title>
+      <SpellsList />
     </>
   );
 };
