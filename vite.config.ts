49b7346bed5f6010a5707ec4763a973bbf932f69
romsendz <sendzyuk.r@gmail.com>
2025-01-11 15:52:46 +0100

    install @types/node; provided aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/hp-world/",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "@styles": resolve(__dirname, "src/styles"),
+      "@components": resolve(__dirname, "src/components"),
+      "@layouts": resolve(__dirname, "src/layouts"),
+      "@pages": resolve(__dirname, "src/pages"),
+      "@assets": resolve(__dirname, "src/assets"),
+    },
+  },
 });
