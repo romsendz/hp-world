0525aba862c50a27f87afd0c35b908646a00cdfa
romsendz <sendzyuk.r@gmail.com>
2025-01-12 15:59:22 +0100

    removed optional components, moved content to @pages

--- src/pages/Books/index.tsx
+++ src/pages/Books/index.tsx
@@ -1,12 +1,43 @@
-import BooksList from "../../components/BooksList";
-import Title from "../../components/Title";
+import useBooksQuery from "../../assets/api/queries/useBooks";
+import ErrorMessage from "../../layouts/ErrorMessage";
+import Loading from "../../layouts/Loading";
+import styles from "./index.module.scss";
 
 const Books = () => {
+  const { data: books, isLoading, isError, error } = useBooksQuery();
+  if (isLoading) return <Loading />;
+  if (isError) return <ErrorMessage error={error} />;
   return (
-    <>
-      <Title>Books</Title>
-      <BooksList />
-    </>
+    <div className={styles.books}>
+      {books?.map((book) => {
+        return (
+          <article key={book.index} className={styles.book}>
+            <h5 className={styles.book__title}>{book.title}</h5>
+            <img
+              src={book.cover}
+              alt={book.title}
+              className={styles.book__image}
+            />
+            <div className={styles.book__details}>
+              <h5 className={styles.book__details__title}>{book.title}</h5>
+              <hr className={styles.book__details__divider} />
+              <p className={styles.book__details__description}>
+                {book.description}
+              </p>
+              <hr className={styles.book__details__divider} />
+              <ul>
+                <li>
+                  Release Date:<b> {book.releaseDate}</b>
+                </li>
+                <li>
+                  Number of pages: <b>{book.pages}</b>
+                </li>
+              </ul>
+            </div>
+          </article>
+        );
+      })}
+    </div>
   );
 };
 
--- src/pages/Houses/index.tsx
+++ src/pages/Houses/index.tsx
@@ -1,12 +1,36 @@
-import HousesList from "../../components/HousesList";
-import Title from "../../components/Title";
+import useHousesQuery from "../../assets/api/queries/useHouses";
+import ErrorMessage from "../../layouts/ErrorMessage";
+import Loading from "../../layouts/Loading";
+import styles from "./index.module.scss";
 
 const Houses = () => {
+  const { data: houses, isLoading, isError, error } = useHousesQuery();
+  if (isLoading) return <Loading />;
+  if (isError) return <ErrorMessage error={error} />;
   return (
-    <>
-      <Title>Houses</Title>
-      <HousesList />
-    </>
+    <div className={styles.houses}>
+      {houses?.map((house) => {
+        return (
+          <article key={house.index}>
+            <h4>{house.house}</h4>
+            <br />
+            <ul>
+              <li>
+                Founder:<b> {house.founder}</b>
+              </li>
+              <li>
+                Animal:
+                <b> {house.animal} </b>
+                <span>{house.emoji}</span>
+              </li>
+              <li>
+                Colors:<b> {house.colors.join(", ")}</b>
+              </li>
+            </ul>
+          </article>
+        );
+      })}
+    </div>
   );
 };
 
