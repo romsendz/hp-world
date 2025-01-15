8a97d697c11cadefc6c0daa1c0b2168926a2af39
romsendz <sendzyuk.r@gmail.com>
2025-01-15 21:18:43 +0100

    i18next intialized / added translations

---
+++ src/locale/i18n.ts
@@ -0,0 +1,34 @@
+import i18next from "i18next";
+import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+import en from "./en.json";
+import fr from "./fr.json";
+import uk from "./uk.json";
+import it from "./it.json";
+import es from "./es.json";
+import pt from "./pt.json";
+
+i18next
+  .use(I18nextBrowserLanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      fr: { translation: fr },
+      uk: { translation: uk },
+      es: { translation: es },
+      it: { translation: it },
+      pt: { translation: pt },
+    },
+    fallbackLng: "en",
+    debug: true,
+    interpolation: {
+      escapeValue: false, // React handles escaping
+    },
+    detection: {
+      order: ["localStorage", "navigator"], // Detect from localStorage first, then browser
+      caches: ["localStorage"], // Persist in localStorage
+    },
+  });
+
+export default i18next;
