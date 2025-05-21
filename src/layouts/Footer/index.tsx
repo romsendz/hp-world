3a90f4d1a5c20c58cdf52f0e61333f36a05cf441
romsendz <sendzyuk.r@gmail.com>
2025-05-21 09:45:12 +0200

    added credits

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -1,4 +1,3 @@
-import GithubLogo from "../../assets/svgs/github.svg?react";
 import Container from "../Container";
 import styles from "./index.module.scss";
 
@@ -6,15 +5,16 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <Container maxWidth="1240px">
-        <a
-          className={styles.credits}
-          href="https://github.com/romsendz/hp-world"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GithubLogo width={"1.5em"} />
-          Credits
-        </a>
+        <span className={styles.credits}>
+          Designed and developed by{" "}
+          <a
+            href="https://portfolio-romsendz.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Roman Sendziuk
+          </a>
+        </span>
       </Container>
     </footer>
   );
